6e316bb95a51b482506ac54c68d0deccd611076b
bczion <bczion@hotmail.com>
2018-06-23 01:02:11 -0300

    estruturas de dados prontas

--- include/pilha.h
+++ include/pilha.h
@@ -1,82 +1,140 @@
-#ifndef Pilha_H
-#define Pilha_H
+/**
+ * @file	pilha.h
+ * @brief	Implementação da classe Pilha em C++
+ * @author	Bruno César Lopes Silva
+ * @since	22/06/2018
+ * @date	22/06/2018
+ */
 
-#include <iostream>
-#include <memory>
+#ifndef PILHA_H
+#define PILHA_H
 
+#include "lista.h"
+
+/* Para permitir sobrecarregar o operador de insercao
+   numa classe template como friend é preciso adicionar
+   o trecho de codigo a seguir */
+template <typename T> class Pilha; // Declaracao antecipada da classe
+template <typename T> // Definicao antecipada do template para o operador de insercao
+std::ostream& operator<<( std::ostream&, Pilha<T> const & );
+// --
+
+/**
+* @brief Implementacao da classe Pilha
+* @detail Define um template de pilha
+* usando como container padrão uma lista
+*/
 template <typename T>
-class Pilha {
-private:
-	T* m_elementos;   // Elementos armazenados na pilha
-	int m_tamanho;    // Qtde de elementos
-	int m_capacidade; // Qtde Max de elementos
-public:
-	Pilha (int n_capacidade = 50);
-	~Pilha ();
-	bool empty ();
-	bool full ();
-	T& top ();
-	int push ( T novo );
-	int pop ();
-	int size();
+class Pilha{
+    ListaLigada<T> pilha; /**< Container para armazenar conteúdo da pilha. */
+
+    public:      
+
+        /**
+        * @brief Método construtor padrão de Pilha
+        */
+        Pilha();
+
+        /**
+        * @brief Método destrutor de Pilha
+        */
+        ~Pilha();
+
+        /**
+        * @brief Método para inserir valores.
+        * @detail Insere valores no topo da pilha.
+        * @return Retorna valor boleano.
+        */
+        bool insereNoTopo( T conteudo );
+
+        /**
+        * @brief Método para remover valores.
+        * @detail Remove o valor no topo da fila.
+        * @return Retorna valor boleano.
+        */
+        bool removeDoTopo();
+
+        /**
+        * @brief Método que retorna o tamanho da pilha
+        */
+        int getTamanho();
+
+        /**
+        * @brief Método que retorna o elemento do topo da pilha
+        * @return Retorna o conteúdo do topo da pilha
+        */
+        T topo();
+
+        /**
+        * @brief Método para verificar se a pilha está vazia
+        * @return Retorna verdadeiro ou falso em relação a pilha vazia
+        */
+        bool evazio();
+
+        /**
+        * @brief Método para imprimir o conteúdo da pilha
+        */
+        void imprimir();
+
+        /**
+        * @brief Sobrecarga do operador de inserção.
+        * @detail imprime na saída padrão as informações da pilha
+        * @param ostream &o operador de inserção
+        * @param Pilha<T> f uma instância de Pilha
+        * @return retorna a instância do operador de inserção
+        */
+        friend std::ostream& operator<< <T>( std::ostream&, Pilha<T> const &p);
+        
 };
 
 template <typename T>
-Pilha<T>::Pilha (int n_capacidade): m_tamanho(0), m_capacidade(n_capacidade)
-{
-	m_elementos = new T[n_capacidade];
+Pilha<T>::Pilha(){}
+
+template <typename T>
+Pilha<T>::~Pilha(){}
+
+template <typename T>
+bool Pilha<T>::insereNoTopo( T conteudo ){
+    this->pilha.insereNoInicio( conteudo );
+
+    return true;
 }
 
 template <typename T>
-Pilha<T>::~Pilha ()
-{
-	delete [] m_elementos;
+bool Pilha<T>::removeDoTopo(){
+    this->pilha.remove(0);
+
+    return true;
 }
 
 template <typename T>
-bool Pilha<T>::empty ()
-{
-	return m_tamanho == 0;
+int Pilha<T>::getTamanho(){
+    return pilha.getTamanho();
 }
 
 template <typename T>
-bool Pilha<T>::full ()
-{
-	return m_tamanho == m_capacidade;
+T Pilha<T>::topo(){
+    return pilha.getPrimeiroElemento();
 }
 
 template <typename T>
-int Pilha<T>::push ( T novo )
-{
-	if (full())
-		return 0;
-	m_elementos[m_tamanho++] = novo;
-	return 1;
-}
-	
-template <typename T>
-int Pilha<T>::pop ()
-{
-	if (empty())
-		return 0;
-	m_tamanho--; 
-	return 1;
+bool Pilha<T>::evazio(){
+    return pilha.vazio();
 }
 
 template <typename T>
-T& Pilha<T>::top ()
-{	
-	if (empty()) {
-		std::cerr << "Acesso invalido a elemento no topo. O programa sera fechado!" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	return m_elementos[m_tamanho-1];
+void Pilha<T>::imprimir(){
+    pilha.imprimir();
 }
 
 template <typename T>
-int Pilha<T>::size ()
-{
-	return m_tamanho;
+std::ostream& operator<< ( std::ostream& o, Pilha<T> const &p) {
+    //auto atual = f.cabeca;
+    //o << f.imprimir();
+/* 
+    for(int i = 0; i < (int)f.getTamanho(); i++)
+        o << f.getElemento(i);
+ */    return o;
 }
 
-#endif // End of Pilha_H
+#endif
